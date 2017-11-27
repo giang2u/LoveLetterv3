7135dbcb9776d8c11613938dc475099189496ecb
Hue_Nam <huenam.ly@gmail.com>
2017-11-27 18:22:14 +0100

    error affichage image)

--- LoveLetter/src/AppBundle/Controller/AdvertController.php
+++ LoveLetter/src/AppBundle/Controller/AdvertController.php
@@ -7,13 +7,20 @@ namespace AppBundle\Controller;
 // N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use  AppBundle\Entity;
 
 class AdvertController extends Controller
-{
-  public function indexAction()
-  {
+{/*
+  public function indexAction(){
     $content = $this->get('templating')->render('base.html.twig');
     
     return new Response($content);
-}
+	}*/
+
+	public function cartesAction(){
+
+        $content = $this->get('templating')->render('LoveLetterPlatformBundle:Advert:cartes.html.twig');
+    	return new Response($content);
+	}
+
 }
