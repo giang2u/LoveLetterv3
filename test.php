546bbc8ef9a342999c372e5cdc1444d754dce2c0
giang <giang.andre@outlook.com>
2017-11-28 15:52:32 +0100

    img entity

---
+++ test.php
@@ -0,0 +1,33 @@
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Création d&#39;un design étape par étape - Etape n°1 : Le code xhtml</title>
+  </head>
+  
+  <body>
+  <div id="conteneur">    
+    <h1 id="header"><a href="#" title="Colored Design - Accueil"><span>Colored Design</span></a></h1>
+
+
+<?php 
+
+include '/LoveLetterv3/LoveLetter/src/AppBundle/Entity/Carte.php';
+
+$c = new Carte("Princess");
+
+$r = rand(1,15);
+
+
+
+$rr = rand(1,15);
+
+echo $r;
+
+echo $rr;
+
+var_dump($c);
+
+?>
+
+ </body>
+</html>
